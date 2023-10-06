00b06874664c51e16237954330819b9fbf368b75
Matthew Awad <z5419355@ad.unsw.edu.au>
2023-10-06 11:55:49 +1100

    initial

---
+++ src/main/java/com/matthewa/pipelineproj/PipelineProjApplication.java
@@ -0,0 +1,13 @@
+package com.matthewa.pipelineproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipelineProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PipelineProjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matthewa/pipelineproj/PipelineProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matthewa.pipelineproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PipelineProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
